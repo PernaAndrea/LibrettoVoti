bc130e1de8bb1ca9a5c5fd7635d6915490c12103
andrea <andrea@localhost>
2021-03-15 12:35:24 +0100

    Creato pattern MVC

--- src/main/java/it/polito/tdp/librettovoti/FXMLController.java
+++ src/main/java/it/polito/tdp/librettovoti/FXMLController.java
@@ -1,44 +1,40 @@
 
-package it.polito.tdp.librettovoti;
+	package it.polito.tdp.librettovoti;
 
-import java.net.URL;
+	import java.net.URL;
 import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
 import java.util.ResourceBundle;
 
-import it.polito.tdp.librettovoti.model.*;
+import it.polito.tdp.librettovoti.model.Libretto;
+import it.polito.tdp.librettovoti.model.Voto;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+	import javafx.fxml.FXML;
+	import javafx.scene.control.DatePicker;
+	import javafx.scene.control.TextArea;
+	import javafx.scene.control.TextField;
 
-
-public class FXMLController {
+	public class FXMLController {
+		
+		private Libretto model= new Libretto();
 	
-	private Libretto model= new Libretto();
-	
 
-    @FXML
-    private ResourceBundle resources;
+	    @FXML
+	    private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+	    @FXML
+	    private URL location;
 
-    @FXML
-    private Label txtEsame;
+	    @FXML
+	    private TextField txtEsame;
 
-    @FXML
-    private Label txtVoto;
+	    @FXML
+	    private TextField txtVoto;
 
-    @FXML
-    private Label txtData;
-    
-    @FXML
-    private DatePicker pickerEsame;
+	    @FXML
+	    private DatePicker pickerEsame;
 
-    @FXML
-    private TextArea txtResult;
+	    @FXML
+	    private TextArea txtResult;
     
     // 3 abbiamo aggiunto la variabile modello e questo metodo 
     public void setModel(Libretto model) {
@@ -58,13 +54,13 @@ public class FXMLController {
     	
     	String votoEsame = txtVoto.getText();
     	int votoInt =0;
-    //	try {
+    	try {
     	votoInt = Integer.parseInt(votoEsame);
-  /*  	}catch( NumberFormatException e) {
+    	}catch( NumberFormatException e) {
     		txtResult.setText("Errore: il voto deve essere numerico");
     		return; 
     	}
-    	*/
+    	
     	if(votoInt<18 || votoInt>30) {
     		txtResult.setText("Errore: il voto deve essere compreso tra 18 e 30");
     		return;
@@ -105,9 +101,8 @@ public class FXMLController {
     void initialize() {
         assert txtEsame != null : "fx:id=\"txtEsame\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtVoto != null : "fx:id=\"txtVoto\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtData != null : "fx:id=\"txtData\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
         assert pickerEsame != null : "fx:id=\"pickerEsame\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
